1929fc9d0cb4fb17e5627411146f2b88fc28dd18
Abhijit Patil <69165799+abhijit-23blaze@users.noreply.github.com>
2025-08-09 00:22:32 +0530

    cache fix

--- constants/ApiKeys.ts
+++
@@ -1,9 +0,0 @@
-
-export const GEMINI_API_KEY = 'AIzaSyBSLl0AbbZJ5mn2at5ZIY-VlRf23Z2TMuA'; 
-
-export const FIREBASE_API_KEY = "AIzaSyCdcEH7BN6A6b-opjCZZxn-kyVM2pnOGoQ";
-export const FIREBASE_AUTH_DOMAIN = "kenzen-x1.firebaseapp.com";
-export const FIREBASE_PROJECT_ID = "kenzen-x1";
-export const FIREBASE_STORAGE_BUCKET = "kenzen-x1.firebasestorage.app";
-export const FIREBASE_MESSAGING_SENDER_ID = "460667022191";
-export const FIREBASE_APP_ID = "1:460667022191:web:019ba3eca760d256c1969a"; 
